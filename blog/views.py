c9d02885764c30426509b9bbc3a8a60d468b678c
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-02-12 22:58:45 +0300

    Added home and about templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Harison Mwangi',
+        'title': 'Blog post 1',
+        'content': 'First post content', 
+        'date_posted': '12 February 2020',
+    },
+
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': '13 February 2020',
+    }
+
+]
 
 # adding a blog home route
 def home(request):
@@ -7,8 +24,15 @@ def home(request):
     Handles traffic from the home page of the blog.
     It returns what we want the user to see when they're sent to this route.
     """
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 # adding a blog about route
+# option 1
+# def about(request):
+#    return HttpResponse("<h1>Blog About</h1>")
+
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html')
