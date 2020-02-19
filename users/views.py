ebf1983a39212d50c91bc60a82e09e47f1c24567
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-02-19 12:40:07 +0300

    Users can now update their profile.
    Created a model form in forms.py, imported the forms to views.py and passed them to the context.
    
    Added the form to profile.html, and changed it's encoding type.
    
    Added feedback if user profile is updated and changed the url that they will be redirected to.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from . forms import UserRegisterForm
+from . forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # creating a register view
 def register(request):
@@ -12,7 +12,7 @@ def register(request):
             form.save() # saves form data when it is valid
             username = form.cleaned_data.get("username")
             # adding a flash message
-            messages.success(request, f"{username}'s account has been created. You are now able to log in.")
+            messages.success(request, f"Your account has been created. You are now able to log in.")
             return redirect("login")
 
     else:
@@ -23,4 +23,27 @@ def register(request):
 # creating a profile view
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        # pass the current user's info to the form
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+        # save the data from both forms only when both inputs are valid
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Your profile has been updated")
+            # avoid the redirect alert from browser when one reloads the page
+            return redirect("profile")
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, "users/profile.html", context)
