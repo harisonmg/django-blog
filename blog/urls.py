a922841426ad928640b02d5f8600b65acafb0821
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-02-21 11:59:47 +0300

    Added post create view
    Added post create view that redirects  to the detailed post view once a post is created.
    
    Added a mixin to ensure a user is logged in to create a post.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 # import views.py from current directory
-from . views import PostListView, PostDetailView
+from . views import (PostListView, 
+    PostDetailView,
+    PostCreateView
+)
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
     # int -> integers only; pk -> primary key
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('about/', views.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Post
 # from django.http import HttpResponse
 
@@ -23,6 +28,16 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # overide the form valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+
+        return super().form_valid(form)
 
 # adding a blog about route
 # option 1
