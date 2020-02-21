157e72a1197b749c8bdb2eb579c0ab1146700a6d
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-02-21 09:37:53 +0300

    Added post list view and post detail view
    Used class-based and detail views for homepage.
    Added url routes for post detail views to homepage.
    
    Changed indenting for home.html
    
    Class-based views:
    1. List views
    2. Detail views
    3. Create views
    4. Update views

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 # import views.py from current directory
+from . views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    # int -> integers only; pk -> primary key
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('about/', views.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 # from django.http import HttpResponse
 
@@ -12,6 +13,16 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'    # default template: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 # adding a blog about route
 # option 1
