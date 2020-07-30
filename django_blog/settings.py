9bdcd4c26c051ade568e9af4fb4c1de7f3be4139
Harison Mwangi <harrisonmwangi288@gmail.com>
2020-07-30 09:08:03 +0300

    Fixed S3 bucket image access issue

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '-kp)54-de9=o%xk7vssj^evfyk_yb!0*!g545a84azgr2=kb94')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '-kp)54-de9=o%xk7vssj^evfyk_yb!0*!g545a84azgr2=kb94')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['django-blog-hm.herokuapp.com']
+ALLOWED_HOSTS = ['django-blog-hm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -145,6 +145,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
